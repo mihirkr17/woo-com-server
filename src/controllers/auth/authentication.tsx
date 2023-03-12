352942a0b526fdc9db084aaff9cc3968c8468c9f
mroy35034 <mroy35034@gmail.com>
2023-03-12 21:20:02 +0600

    secure true to false in auth

--- src/controllers/auth/authentication.tsx
+++ src/controllers/auth/authentication.tsx
@@ -253,8 +253,8 @@ module.exports.loginController = async (req: Request, res: Response, next: NextF
 
       if (token) {
          res.cookie("token", token, cookieObject);
-         res.cookie("u_data", userDataToken, { httpOnly: false, maxAge: 57600000, sameSite: "none", secure: true });
-         res.cookie("uid", existUser?._UUID, { httpOnly: false, maxAge: 57600000, sameSite: "none", secure: true });
+         res.cookie("u_data", userDataToken, { httpOnly: false, maxAge: 57600000, sameSite: "none", secure: false });
+         res.cookie("uid", existUser?._UUID, { httpOnly: false, maxAge: 57600000, sameSite: "none", secure: false });
 
          // if all success then return the response
          return res.status(200).send({ name: "isLogin", message: "LoginSuccess", uuid: existUser?._UUID });
